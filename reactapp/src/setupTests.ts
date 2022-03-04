89fabd01cb67e2f32f2edfe03e9be98aca59f47a
Parvathy Hari <parvathyvhari@gmail.com>
2022-03-04 17:28:53 +0530

    fix: modified script name for test,coverage and lint reports

--- reactapp/src/setupTests.ts
+++ reactapp/src/setupTests.ts
@@ -3,5 +3,5 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-console.log("to make linting error");
-console.log("to make linting error")
+// console.log("to make linting error");
+// console.log("to make linting error")
